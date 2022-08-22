d49beff608c7825c9e136c2cba8184435032b06a
Vital-stude <110226252+Vital-stude@users.noreply.github.com>
2022-08-22 23:43:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+   ;public static void main(String[] args) {
+
+       int[] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");}
+        System.out.println(" ");
+            for (int r = arr.length - 1; r >= 0 ; r--) {
+                System.out.print(arr[r] + " ");}
+           System.out.println(" ");
+            {double[] doubleBear = new double[3];
+        doubleBear[0] = 1.57;
+        doubleBear[1] = 7.654;
+        doubleBear[2] = 9.986;
+        for (int t = 0; t < 3; t++) {
+            System.out.print(doubleBear[t] + "," + "  ");}
+        System.out.println("   ");
+        for (int t = 2; t >=0; t--) {
+            System.out.print(doubleBear[t] + "," + "  ");}
+
+        System.out.println(" ");}
+
+            {int[] rem = new int[4];
+        rem[0] = 13323;
+        rem[1] = 1415;
+        rem[2] = 8987;
+        rem[3] = 790;
+        for (int g = 0; g < rem.length; g++) {
+            System.out.print(rem[g] + ","+" ");}
+                System.out.println(" ");
+        for (int g = 3; g>=0; g--) {
+            System.out.print(rem[g] + ", " + " ");}
+        System.out.println(" ");   {
+
+}}}
+
+}
